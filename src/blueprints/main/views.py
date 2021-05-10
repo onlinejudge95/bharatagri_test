10844c65d469fd55cafc32cb534fb727de3ac5af
onlinejudge95 <onlinejudge95@gmail.com>
2021-05-10 15:46:06 +0530

    Adds celery task in main blueprint

--- src/blueprints/main/views.py
+++ src/blueprints/main/views.py
@@ -2,16 +2,18 @@ from flask import Blueprint
 from flask import jsonify
 
 from config import settings
-from src.blueprints.main.decorator import exponential_backoff
 
 
 bp = Blueprint("main", __name__, url_prefix="/api")
 
 
 @bp.route("/")
-# @exponential_backoff(retries=3)
 def simulated_process():
     # To simulate a faulty/overloaded service
     circuit_breaker = settings.CIRCUIT_BREAKER_FLAG
+
+    from src.blueprints.main.tasks import simulated_task
+    simulated_task.delay(circuit_breaker, retries=3)
     
+    return jsonify({"status": "ok", "message": "Task scheduled"})
     
