a8f0ab05dfda80bc2ed2e0a33b1aa3c9f570d851
onlinejudge95 <onlinejudge95@gmail.com>
2021-05-10 14:24:03 +0530

    Regsiters mainn blueprint

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
 from src.blueprints.health import health_bp
+from src.blueprints.main import main_bp
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object("config.settings")
 
     app.register_blueprint(health_bp)
+    app.register_blueprint(main_bp)
 
     return app
